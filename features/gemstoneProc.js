85d49b25aaa093b44500fc59aec27ab9ee960b7c
XandroVdv <102529867+XandroVdv@users.noreply.github.com>
2024-07-10 19:15:00 +0200

    add: averare pristine

--- features/gemstoneProc.js
+++ features/gemstoneProc.js
@@ -1,10 +1,24 @@
-// &r&d&lPRISTINE! &r&fYou found &r&a❤ Flawed Ruby Gemstone &r&8x1&r&f!&r
 let displayProcMessage = true;
+let pristineProcs = [];
+let totalProcs = 0;
 
 register("chat", (event) => {
-    if(!displayProcMessage) {
+    let message = ChatLib.getChatMessage(event);
+
+    if (!displayProcMessage) {
         cancel(event);
         console.log("cancelled gemstone proc message");
+    }
+
+    if (message.includes("PRISTINE!") && message.includes("You found")) {
+        let match = message.match(/You found [^x]+x(\d+)/);
+        if (match && match[1]) {
+            let quantity = parseInt(match[1]);
+            pristineProcs.push(quantity);
+            totalProcs += quantity;
+            let averageProcs = totalProcs / pristineProcs.length;
+            ChatLib.chat(`&6[MissingFeatures] &7Pristine proc message detected! Average: &r&a${averageProcs.toFixed(2)}&r&7.`);
+        }
     }
 }).setCriteria("&r&d&lPRISTINE! &r&fYou found ${*}");
 
