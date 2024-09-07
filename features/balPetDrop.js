af4b8db73753522dc68898ecc3b909beda4d83ef
XandroVdv <xandro.minnie@gmail.com>
2024-09-07 23:34:41 +0200

    fix bal pets

--- features/balPetDrop.js
+++ features/balPetDrop.js
@@ -6,16 +6,17 @@ let balPetDropDisplay = true;
 if (balPetDropDisplay) {   
     register("chat", (event) => {
         let message = ChatLib.getChatMessage(event);
-         
-        if(message == "&r&6&lRARE DROP! &r&eA Bal Pet dropped!&r") {        
-        let toDisplay = "§r§eEasy bal pet!§r";
-        // Calculate x and y coordinates to center the text
-        let textWidth = Renderer.getStringWidth(toDisplay);
-        let x = (screenWidth / 2) - (textWidth / 2); 
-        let y = screenHeight / 2 - 10;  
         
-        // Draw the string on the screen
-        Renderer.drawString(toDisplay, x, y);
+        if(message == "&r&6&lRARE DROP! &r&eA Bal Pet dropped!&r") {        
+            let toDisplay = "§r§eEasy bal pet!§r";
+            // Calculate x and y coordinates to center the text
+            let textWidth = Renderer.getStringWidth(toDisplay);
+            let x = (screenWidth / 2) - (textWidth / 2); 
+            let y = screenHeight / 2 - 10;  
+            
+            cancel(event);
+            // Draw the string on the screen
+            Renderer.drawString(toDisplay, x, y);
         }
     });
 }
